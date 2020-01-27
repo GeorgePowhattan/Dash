b8df3df41ab55d1438f768cbf4b0e6083ca06280
GeorgePowhattan <martinek.ondrej@gmail.com>
2020-01-27 22:14:52 +0100

    update

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 import dash
+import dash_html_components as html
+import dash_core_components as dcc
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app.layout = html.Div([
+    dcc.Tabs(id="tabs", value='tab-1', children=[
+        dcc.Tab(label='Tab one', value='tab-1'),
+        dcc.Tab(label='Tab two', value='tab-2'),
+    ]),
+    html.Div(id='tabs-content')
+])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
