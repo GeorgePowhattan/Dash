0f5e536aa8510a470455dc0ac7198cbab9b691c0
Ondřej Martínek <46747310+GeorgePowhattan@users.noreply.github.com>
2020-02-26 16:11:08 +0100

    update

--- data.py
+++ data.py
@@ -8,7 +8,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, Integer, String
 
-engine = sqlalchemy.create_engine('sqlite:///:memory:', echo=True)
+
+engine = sqlalchemy.create_engine('sqlite:///site.db', echo=True)
 
 Base = declarative_base()
 
@@ -16,18 +17,19 @@ session = sessionmaker(bind=engine)()
 
 
 def read_data(url):
-    loans_url = 'https://www.cnb.cz/cnb/STAT.ARADY_PKG.VYSTUP?p_period=1&p_sort=1&p_des=50&p_sestuid=1538&p_uka=1%2C2%2C3&p_strid=AAD&p_od=200512&p_do=209912&p_lang=CS&p_format=2&p_decsep=.'
-
-    df = pd.read_csv()
-
+    df = pd.read_csv(url, sep='|',skiprows=0,verbose=False,encoding ='utf-8',header=0)
+    df.iloc[:,0] = pd.to_datetime(df.iloc[:,0])
     return df
 
-def create_database
+
+class Loans(Base):
+    id = Column(Integer,primary_key=True)
+    date = Column(Float,)
+    date = Column(Float,)
 
 
+if __name__ == '__main__':
 
+    loans_url = 'https://www.cnb.cz/cnb/STAT.ARADY_PKG.VYSTUP?p_period=1&p_sort=1&p_des=50&p_sestuid=1538&p_uka=1%2C2%2C3&p_strid=AAD&p_od=200512&p_do=209912&p_lang=CS&p_format=2&p_decsep=.'
 
-
-
-
-
+    loans = read_data(url)
