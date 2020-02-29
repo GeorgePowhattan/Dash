1b22bed90782c4341a00286b829fe4adca0d567f
GeorgePowhattan <martinek.ondrej@gmail.com>
2020-02-29 23:03:06 +0100

    update

--- data.py
+++ data.py
@@ -21,7 +21,7 @@ def read_data(url):
     df.iloc[:,0] = pd.to_datetime(df.iloc[:,0])
     return df
 
-
+# db tables:
 class Loans(Base):
     id = Column(Integer,primary_key=True)
     date = Column(Float,)
@@ -30,6 +30,9 @@ class Loans(Base):
 
 if __name__ == '__main__':
 
-    loans_url = 'https://www.cnb.cz/cnb/STAT.ARADY_PKG.VYSTUP?p_period=1&p_sort=1&p_des=50&p_sestuid=1538&p_uka=1%2C2%2C3&p_strid=AAD&p_od=200512&p_do=209912&p_lang=CS&p_format=2&p_decsep=.'
+    url = 'https://www.cnb.cz/cnb/STAT.ARADY_PKG.VYSTUP?p_period=1&p_sort=1&p_des=50&p_sestuid=1538&p_uka=1%2C2%2C3&p_strid=AAD&p_od=200512&p_do=209912&p_lang=CS&p_format=2&p_decsep=.'
 
+    # Get the df:
     loans = read_data(url)
+
+    # Nasypat df do db: Jak manipuovat s celymi radky?
